4503c79fd82d800dba9dd5c00e56d396418c0ec9
Shakthi Vel K <shakthivelk1124@gmail.com>
2025-10-15 20:17:08 +0530

    Update db.mjs

--- Pro2/db.mjs
+++ Pro2/db.mjs
@@ -1,7 +1,7 @@
 import mongoose from "mongoose"; 
 const connetDB = async() => { 
 try{
-     await   mongoose.connect('mongodb+srv://shakthivelk1124_db_user:pB18dN1deyZxHPTV@cluster0.vdqcy1f.mongodb.net/TestDB') 
+     await   mongoose.connect('mongodb+srv://<username>:<password>@cluster0.xxxxxx.mongodb.net/TestDB') 
 console.log('conneted to Database.....') 
 }catch(error)
 { 
