0027e7194d2e23a8f4a1bd425747b39e5b6fb26f
Shakthivelk24 <shakthivelk1124@gmail.com>
2025-10-16 22:06:03 +0530

    IA 2 paper answer

---
+++ IAPaper/Paper21/db.mjs
@@ -0,0 +1,11 @@
+import mongoose from "mongoose"; 
+const connetDB = async() => { 
+try{
+     await   mongoose.connect('mongodb+srv://shakthivelk1124_db_user:7oeNS8Dctko8d9FF@cluster0.5xuxpin.mongodb.net/TestDB') 
+console.log('conneted to Database.....') 
+}catch(error)
+{ 
+      console.log(error)
+ }
+ }
+export default connetDB
