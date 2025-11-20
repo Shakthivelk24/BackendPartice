2ea83a88518d1bdc589744df3f77bc9ed1041c17
Shakthivelk24 <shakthivelk1124@gmail.com>
2025-11-20 19:01:55 +0530

    server

---
+++ Pro4/app.mjs
@@ -0,0 +1,17 @@
+import express from "express";
+import connectDB from "./db.mjs";
+import itemRoutes from "./routes/itemRoutes.mjs";
+
+const app = express();
+app.use(express.json());
+
+// DB Connect
+connectDB();
+
+// Routes
+app.use("/items", itemRoutes);
+
+// Start server
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
+});
